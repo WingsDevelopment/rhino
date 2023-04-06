a2186e61d4dcba76732bfa7c2457c2a3bcc4d0d9
Srdjan Rakic <srdjanr.1609@gmail.com>
2023-04-07 01:02:55 +0200

    hacking id props

--- src/components/fields/DefaultReadOnlyTextField.ts
+++ src/components/fields/DefaultReadOnlyTextField.ts
@@ -12,6 +12,12 @@ export const DefaultReadOnlyTextField = ({ propertyName, label }: Props) => {
   if (propertyName === "id") return "";
   if (propertyName === "Id") return "";
   if (propertyName === "ID") return "";
+  if (label.includes("id")) return "";
+  if (label.includes("Id")) return "";
+  if (propertyName.includes("id")) return "";
+  if (propertyName.includes("Id")) return "";
+  if (propertyName.includes("ID")) return "";
+
   return `
     <${rsc.DefaultReadOnlyTextField} 
         value={${propertyName}}
